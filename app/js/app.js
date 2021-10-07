ace2fba4a6fd5eed70208c77b25a6518beb8fce9
Angel Rafael Rodriguez Hernandez <angel01rdz@outlook.com>
2021-10-06 21:58:45 -0500

    10/06/2021 changes

--- app/js/app.js
+++ app/js/app.js
@@ -1,6 +1,8 @@
 const hamburgerBtn = document.querySelector('.hamburger');
 const header = document.querySelector('header');
 const overlay = document.querySelector('.overlay');
+const tabPanels = document.querySelectorAll('.tab_panel');
+const tabBtns = document.querySelectorAll('.tab_btn');
 
 hamburgerBtn.addEventListener('click', () => {
     if(header.classList.contains('open')) {
@@ -11,3 +13,17 @@ hamburgerBtn.addEventListener('click', () => {
         overlay.style.top = '114px';
     }
 })
+
+function showPanel(panelIndex) {
+    tabBtns.forEach(function(node) { 
+        node.classList.remove('active');
+    });
+    tabBtns[panelIndex].classList.add('active');
+
+    tabPanels.forEach(function(node) {
+        node.style.display = 'none';
+    });
+    tabPanels[panelIndex].style.display = 'flex';
+}
+
+showPanel(0);
